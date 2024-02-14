9666bd88bab802b8580975b532716ae3ea32a6b5
willk0814 <wkoenig0814@gmail.com>
2024-02-14 12:25:40 -0500

    github pages library installed

--- src/ContentPages/EducationPage.js
+++ src/ContentPages/EducationPage.js
@@ -9,13 +9,21 @@ export default function EducationPage() {
           <div className='blurredContentContainer m-2 p-4 max-w-[450px] w-[90vw] items-start justify-start'>
             <h1 className='text-white text-4xl'>Stevens</h1>
             <p className='text-white italic text-bold text-xl'>M.S. in Machine Learning</p>
-            <p className='text-white'></p>
+            <ul className='pl-4 list-disc text-white'>
+              <li>Expected Graduation date: December 2024</li>
+              <li>Part time degree track that allows me to remain elligible for full-time work</li>
+              <li>4.0 GPA through all completed courses</li>
+            </ul>
           </div>
 
           <div className='blurredContentContainer m-2 p-4 max-w-[450px] w-[90vw] items-start justify-start'>
             <h1 className='text-white text-4xl'>University of Delaware</h1>
-            <p className='text-white italic text-bold text-xl'>B.S. in CS, Minor in CyberSecurity</p>
-            <p className='text-white'></p>
+            <p className='text-white italic text-bold text-xl'>B.S. in Computer Science</p>
+            <ul className='pl-4 list-disc text-white'>
+              <li>Minor in CyberSecurity</li>
+              <li>Concentrated degree in Artificial Intelligence & Machine Learning</li>
+              <li>5 time Deans List honoree</li>
+            </ul>
           </div>
 
         </div>
