1fa52512d83f53fb10cb3d86ffc4c1feea64c02a
willk0814 <wkoenig0814@gmail.com>
2024-02-23 15:30:06 -0500

    Navbar with current page indicators

---
+++ src/Components/NavBar/FullNavBar.js
@@ -0,0 +1,127 @@
+import React, { useState, useEffect } from 'react'
+
+import { Link, useLocation } from 'react-router-dom'
+
+export default function FullNavBar() {
+
+    const [showMobileNavBar, setShowMobileNavBar] = useState(false)
+
+    const location = useLocation()
+
+    // SV to hold underlined state for full screen NavBar
+    const [underlineState, setUnderlineState] = useState({
+        home: false,
+        about: false,
+        skills: false,
+        education: false,
+        experience: false,
+        projects: false,
+        contact: false
+    })
+
+    const [currentLocation, setCurrentLocation] = useState('')
+
+    // Functions to toggle underlines for full screen NavBar
+    const toggleUnderline = (variable) => {
+        // confirm that the desired location is not the current location
+        if (variable !== currentLocation){
+            setUnderlineState((prevState) => ({
+                ...prevState,
+                [variable]: !prevState[variable]
+            }))
+        }
+    }
+
+    // Use Effect to handle changes in location
+    useEffect(() => {
+        // isolate and store the current location
+        const current_location = location.pathname.substring(1)
+        setCurrentLocation(current_location)
+
+        // set underline State -> all other locs to false, current Location to true
+        setUnderlineState(prevState => {
+            const newState = { ...prevState };
+            Object.keys(newState).forEach(key => {
+                if (key === current_location){
+                    newState[key] = true
+                } else {
+                newState[key] = false;
+                }
+            });
+            return newState;
+        });
+        
+    }, [location])
+
+  return (
+    <div 
+        className='blurredContentContainer rounded-xl py-0 flex items-center'>
+        <ul className='flex flex-row list-none'>
+            <li 
+                className='px-2'
+                onMouseEnter={() => toggleUnderline('skills')} 
+                onMouseLeave={() => toggleUnderline('skills')}>
+                <Link to='/skills'>
+                    <h2 className='text-white cursor-pointer font-semibold text-2xl'>Skills</h2>
+                </Link>
+                <div 
+                    className='w-[0%] relative bg-white h-[2px] duration-300'
+                    style={{ width: underlineState.skills ? "100%" : "0%"}}>
+                </div>
+            </li>
+
+            <li 
+                className='px-2'
+                onMouseEnter={() => toggleUnderline('education')} 
+                onMouseLeave={() => toggleUnderline('education')}>
+                <Link to='/education'>
+                    <h2 className='text-white cursor-pointer font-semibold text-2xl'>Education</h2>
+                </Link>
+                <div 
+                    className='w-[0%] relative bg-white h-[2px] duration-300'
+                    style={{ width: underlineState.education ? '100%' : '0%'}}>
+                </div>
+            </li>
+
+            <li 
+                className='px-2'
+                onMouseEnter={() => toggleUnderline('experience')} 
+                onMouseLeave={() => toggleUnderline('experience')}>
+                <Link to='/experience'>
+                    <h2 className='text-white cursor-pointer font-semibold text-2xl'>Experience</h2>
+                </Link>
+                <div 
+                    className='w-[0%] relative bg-white h-[2px] duration-300'
+                    style={{ width: underlineState.experience ? '100%' : '0%'}}>
+                </div>
+            </li>
+
+            <li 
+                className='px-2'
+                onMouseEnter={() => toggleUnderline('projects')} 
+                onMouseLeave={() => toggleUnderline('projects')}>
+                <Link to='/projects'>
+                    <h2 className='text-white cursor-pointer font-semibold text-2xl'>Projects</h2>
+                </Link>
+                <div 
+                    className='w-[0%] relative bg-white h-[2px] duration-300'
+                    style={{ width: underlineState.projects ? '100%' : '0%'}}>
+                </div>
+            </li>
+
+            {/* <li 
+                className='px-2'
+                onMouseEnter={() => toggleUnderline('contact')} 
+                onMouseLeave={() => toggleUnderline('contact')}>
+                <Link to='/contact'>
+                    <h2 className='text-white cursor-pointer font-semibold text-2xl'>Contact Me</h2>
+                </Link>
+                <div 
+                    className='w-[0%] relative bg-white h-[2px] duration-300'
+                    style={{ width: underlineState.contact ? '100%' : '0%'}}>
+                </div>
+            </li> */}
+        </ul>  
+    </div> 
+  )
+}
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,20 +1,19 @@
 import React, { useEffect, useState } from 'react'
 
 import { motion } from 'framer-motion'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
+// Import Full & Mobile NavBars
+import FullNavBar from './FullNavBar'
 import MobileNavBar from './MobileNavBar'
 
 export default function NavBar() {
+
+    const location = useLocation()
+
     // SV to hold underlined state for full screen NavBar
     const [underlineState, setUnderlineState] = useState({
         home: false,
-        about: false,
-        skills: false,
-        education: false,
-        experience: false,
-        projects: false,
-        contact: false
     })
 
     // SV to determine which navbar to display
@@ -37,12 +36,12 @@ export default function NavBar() {
                 setShowMobileNavBar(false)
             }
         }
-
         window.addEventListener('resize', handleResize)
         handleResize()
         return (() => window.removeEventListener('resize', handleResize))
     }, [])
-
+    
+    // useEffect to confirm location path
   return (
     <motion.div 
         className='fixed left-0 w-full h-20 z-10 bg-[#2A3C3C]'
@@ -60,6 +59,7 @@ export default function NavBar() {
                         Will Koenig
                     </h2>
                 </Link>
+                
                 {!showMobileNavBar && <div 
                     className='w-[0%] relative bg-white h-[2px] duration-300 left-0 self-start'
                     style={{ width: underlineState.home ? '100%' : '0%'}}>
@@ -68,76 +68,7 @@ export default function NavBar() {
             </div>
             {/* Full Screen NavBar */}
             {!showMobileNavBar ? (
-                <div 
-                    className='blurredContentContainer rounded-xl py-0 flex items-center'
-                    style={{display: !showMobileNavBar? '' : 'none'}}>
-                    <ul className='flex flex-row list-none'>
-                        <li 
-                            className='px-2'
-                            onMouseEnter={() => toggleUnderline('skills')} 
-                            onMouseLeave={() => toggleUnderline('skills')}>
-                            <Link to='/skills'>
-                                <h2 className='text-white cursor-pointer font-semibold text-2xl'>Skills</h2>
-                            </Link>
-                            <div 
-                                className='w-[0%] relative bg-white h-[2px] duration-300'
-                                style={{ width: underlineState.skills ? "100%" : "0%"}}>
-                            </div>
-                        </li>
-
-                        <li 
-                            className='px-2'
-                            onMouseEnter={() => toggleUnderline('education')} 
-                            onMouseLeave={() => toggleUnderline('education')}>
-                            <Link to='/education'>
-                                <h2 className='text-white cursor-pointer font-semibold text-2xl'>Education</h2>
-                            </Link>
-                            <div 
-                                className='w-[0%] relative bg-white h-[2px] duration-300'
-                                style={{ width: underlineState.education ? '100%' : '0%'}}>
-                            </div>
-                        </li>
-
-                        <li 
-                            className='px-2'
-                            onMouseEnter={() => toggleUnderline('experience')} 
-                            onMouseLeave={() => toggleUnderline('experience')}>
-                            <Link to='/experience'>
-                                <h2 className='text-white cursor-pointer font-semibold text-2xl'>Experience</h2>
-                            </Link>
-                            <div 
-                                className='w-[0%] relative bg-white h-[2px] duration-300'
-                                style={{ width: underlineState.experience ? '100%' : '0%'}}>
-                            </div>
-                        </li>
-
-                        <li 
-                            className='px-2'
-                            onMouseEnter={() => toggleUnderline('projects')} 
-                            onMouseLeave={() => toggleUnderline('projects')}>
-                            <Link to='/projects'>
-                                <h2 className='text-white cursor-pointer font-semibold text-2xl'>Projects</h2>
-                            </Link>
-                            <div 
-                                className='w-[0%] relative bg-white h-[2px] duration-300'
-                                style={{ width: underlineState.projects ? '100%' : '0%'}}>
-                            </div>
-                        </li>
-
-                        {/* <li 
-                            className='px-2'
-                            onMouseEnter={() => toggleUnderline('contact')} 
-                            onMouseLeave={() => toggleUnderline('contact')}>
-                            <Link to='/contact'>
-                                <h2 className='text-white cursor-pointer font-semibold text-2xl'>Contact Me</h2>
-                            </Link>
-                            <div 
-                                className='w-[0%] relative bg-white h-[2px] duration-300'
-                                style={{ width: underlineState.contact ? '100%' : '0%'}}>
-                            </div>
-                        </li> */}
-                    </ul>  
-                </div> 
+                <FullNavBar />
                     ) : (
                 <MobileNavBar /> )}
         </div>
